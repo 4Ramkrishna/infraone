f0ab8aaf7d900badea3832d7ea4a444fcbfd4e67
Ramkrishna Patidar <ramkrishnapatidar@Ramkrishnas-Air.lan>
2026-01-01 22:43:22 +0530

    added lombok dependency

--- src/main/java/io/infraone/backend_core/payload/response/MessageResponse.java
+++ src/main/java/io/infraone/backend_core/payload/response/MessageResponse.java
@@ -1,10 +1,11 @@
 package io.infraone.backend_core.payload.response;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class MessageResponse {
     private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
 
     public String getMessage() {
         return message;
