d42c46e18d5981c95b3a7178a9ec1606d14a5130
Ramkrishna Patidar <ramkrishnapatidar@Ramkrishnas-Air.lan>
2026-01-01 23:42:55 +0530

    necessary changes required

--- src/main/java/io/infraone/backend_core/security/jwt/AuthTokenFilter.java
+++ src/main/java/io/infraone/backend_core/security/jwt/AuthTokenFilter.java
@@ -30,6 +30,15 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        String requestPath = request.getServletPath();
+
+        // do not process JWT for auth endpoints
+        if (requestPath.contains("/api/auth/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         try {
             String jwt = parseJwt(request);
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
